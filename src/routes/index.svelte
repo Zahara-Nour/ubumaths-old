619f9e8bbda6f7d44e53b8686c274257ad45f69d
David Le Jolly <zahara.alnour@gmail.com>
2022-05-25 07:50:00 +0300

    Add smui

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,11 @@
+<script>
+    import Button , {Label} from '@smui/button';
+  </script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+
+<Button on:click={() => {}} variant="raised">
+    <Label>Raised</Label>
+  </Button>
