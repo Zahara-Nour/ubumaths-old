089ae2930d052f6e97e5a1089af6a9a55020c288
David Le Jolly <zahara.alnour@gmail.com>
2022-05-25 08:02:30 +0300

    Add mathlive

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,11 +1,36 @@
 <script>
-    import Button , {Label} from '@smui/button';
-  </script>
+	import Button, { Label } from '@smui/button';
+	import { onMount } from 'svelte';
+
+	let markup = '';
+	let toMarkup;
+	let ready;
+
+	onMount(async () => {
+		import('https://unpkg.com/mathlive/dist/mathlive.min.mjs')
+			.then((m) => {
+				toMarkup = m.convertLatexToMarkup;
+				ready = true;
+			})
+			.catch((e) => {
+				console.log('erreur', e);
+			});
+	});
+
+	$: if (toMarkup) {
+		markup = toMarkup('\\frac{3}{4}');
+	}
+</script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
-
 <Button on:click={() => {}} variant="raised">
-    <Label>Raised</Label>
-  </Button>
+	<Label>Raised</Label>
+</Button>
+
+{@html markup}
+
+{#if ready}
+	<math-field virtual-keyboard-mode="manual" />
+{/if}
