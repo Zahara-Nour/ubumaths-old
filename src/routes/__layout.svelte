c385580bbf380b1b39430247185370d637ff093f
David Le Jolly <zahara.alnour@gmail.com>
2022-05-25 09:10:31 +0300

    add appbar, drawer and darkmode

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,97 @@
+<script context="module">
+
+</script>
+
+<script>
+	import TopAppBar, { Row, Section, Title as TitleBar } from '@smui/top-app-bar'
+	import IconButton from '@smui/icon-button'
+	import Drawer, {
+		AppContent,
+		Content,
+		Header,
+		Title,
+		Subtitle,
+		Scrim,
+	} from '@smui/drawer'
+	import { darkmode } from '../lib/stores'
+	import { onMount } from 'svelte'
+
+	let prominent = false
+	let dense = false
+	let secondaryColor = false
+	let miniWindow = false
+	let showDrawer = false
+	const setMiniWindow = () => (miniWindow = window.innerWidth < 720)
+	const toggleDrawer = () => {
+		showDrawer = !showDrawer
+	}
+	const switchTheme = () => darkmode.update((mode) => !mode)
+
+
+	onMount(() => setMiniWindow())
+</script>
+
+<svelte:window on:resize="{setMiniWindow}" />
+<svelte:head>
+	{#if $darkmode}
+		<link rel="stylesheet" href="/smui-dark.css" />
+	{:else}
+		<link rel="stylesheet" href="/smui.css" />
+	{/if}
+</svelte:head>
+
+<Drawer variant="modal" bind:open="{showDrawer}">
+	<Header>
+		<TitleBar>Ubumaths</TitleBar>
+		<Subtitle>Les maths de la chandelle verte</Subtitle>
+	</Header>
+	<Content>
+	
+	</Content>
+</Drawer>
+
+<Scrim />
+<AppContent class="app-content">
+	<TopAppBar
+		variant="static"
+		prominent="{prominent}"
+		dense="{dense}"
+		color="{secondaryColor ? 'secondary' : 'primary'}"
+	>
+		<Row>
+			<Section>
+				<Title>Ubumaths</Title>
+			</Section>
+			<Section align="end" toolbar>
+				<IconButton class="material-icons" aria-label="Download"
+					>file_download</IconButton
+				>
+				<IconButton class="material-icons" aria-label="Print this page"
+					>print</IconButton
+				>
+				{#if $darkmode}
+					<IconButton
+						class="material-icons"
+						aria-label="Bookmark this page"
+						on:click="{switchTheme}">light_mode</IconButton
+					>
+				{:else}
+					<IconButton
+						class="material-icons"
+						aria-label="Bookmark this page"
+						on:click="{switchTheme}">dark_mode</IconButton
+					>
+				{/if}
+				{#if miniWindow}
+					<IconButton on:click="{toggleDrawer}" class="material-icons"
+						>menu</IconButton
+					>
+				{/if}
+			</Section>
+		</Row>
+	</TopAppBar>
+	<slot />
+</AppContent>
+
+<style>
+</style>
